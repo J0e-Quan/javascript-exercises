f91f0fcd9bba1a969bd4308d890f44745dc5efb1
Lim Joe Quan <joequanlim@gmail.com>
2025-12-23 18:44:35 +0800

    Complete exercise 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  arr.forEach(element => {
+    total = total + element;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((multiply, current) => multiply * current )
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return (base ** exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1
+	for (i = num; i > 0; i--) {
+    total = total * i
+  }
+  return total;
 };
 
 // Do not edit below this line
