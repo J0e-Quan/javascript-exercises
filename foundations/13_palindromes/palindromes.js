a975c00dac84f5627e45c5a8510d3dd424d6f178
Lim Joe Quan <joequanlim@gmail.com>
2025-12-23 23:21:31 +0800

    Complete exercise 13_palindromes and progressing with exercise 14_fibonacci

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let originalString = string.toLowerCase()
+    let arr = originalString.split("");
+    let filteredArr = arr.filter((item => /^[\p{L}\p{N}]$/u.test(item)))
+    let originalFilteredString = filteredArr.join("");
+    let rra=[]
+    for (i = filteredArr.length; i >-1; i--) {
+        rra.push(filteredArr[i])
+    }
+    let reversedString = rra.join("")
+    if (originalFilteredString === reversedString) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let number = Number(n)
+    let arr = [1, 1,]
+    for (i = 1; i <= number; i++) {
+        let sum = arr[i] + arr[(i-1)]
+        arr.push(sum)
+    }
+    return arr[number]
 };
 
 // Do not edit below this line
